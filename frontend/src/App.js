ab89a777568475825f34d5db27b60333da302369
DEEPIKAS22CSR040 <deepikas.22cse@kongu.edu>
2024-11-10 20:29:55 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import WorkoutHistory from './components/WorkoutHistory';
@@ -5,12 +6,12 @@ import WorkoutStats from './components/WorkoutStats';
 import ExploreExercises from './components/ExploreExercises';
 import VideoTutorials from './components/VideoTutorials';
 import FindFood from './components/FindFood';
-import FindCalories from './components/FindCalories';  // Import the new component
+import FindCalories from './components/FindCalories';
 import './App.css';
 
 function App() {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-  const [activeTab, setActiveTab] = useState('workoutHistory');
+  const [activeTab, setActiveTab] = useState('home'); // Set default tab to 'home'
 
   return (
     <div>
@@ -19,12 +20,12 @@ function App() {
           <header className="app-header">
             <h1 className="app-title">Fit Track</h1>
             <nav className="nav-buttons">
-             <button onClick={() => setActiveTab('findCalories')}>Log Workout</button>
+              <button onClick={() => setActiveTab('findCalories')}>Log Workout</button>
               <button onClick={() => setActiveTab('workoutHistory')}>View History</button>
               <button onClick={() => setActiveTab('workoutStats')}>Statistics & Analytics</button>
               <button onClick={() => setActiveTab('exploreExercises')}>Explore Exercises</button>
               <button onClick={() => setActiveTab('videoTutorials')}>Video Tutorials</button>
-              <button onClick={() => setActiveTab('findFood')}>Find Food</button>              
+              <button onClick={() => setActiveTab('findFood')}>Find Food</button>
               <button onClick={() => logout({ returnTo: window.location.origin })}>Logout</button>
             </nav>
           </header>
@@ -34,7 +35,7 @@ function App() {
           {activeTab === 'workoutHistory' && <WorkoutHistory />}
           {activeTab === 'workoutStats' && <WorkoutStats />}
           {activeTab === 'exploreExercises' && <ExploreExercises />}
-          {activeTab === 'videoTutorials' && <VideoTutorials />}          
+          {activeTab === 'videoTutorials' && <VideoTutorials />}
           {activeTab === 'findCalories' && <FindCalories />}
         </div>
       ) : (
@@ -48,7 +49,7 @@ function App() {
             <div className="login-details">
               <h2>Please log in to track your workouts.</h2>
               <button onClick={loginWithRedirect} className="login-button">
-                Log In
+                Log In / Sign Up
               </button>
             </div>
           </div>
